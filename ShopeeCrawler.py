1be6a55cef5c3d8e11adcf2bfaaaba85d698aec9
anthony2704 <anthony2742000@gmail.com>
2020-11-29 20:45:57 +0700

    delete dataoutput.csv & rewrite ShopeeCrawler.py

--- ShopeeCrawler.py
+++ ShopeeCrawler.py
@@ -1,9 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-import csv
 
-f = open('dataoutput.csv', 'w', newline='')
-writer = csv.writer(f)
 
 chrome_options = Options()
 # chrome_options.add_argument("--headless")
@@ -15,7 +12,6 @@ driver.get("https://shopee.vn/Laptop-cat.13030.13065")
 all_links = driver.find_elements_by_class_name('_3eufr2')
 
 for x in all_links:
-    writer.writerow(x.text)
     print(x.text)
     print('\n')
 
