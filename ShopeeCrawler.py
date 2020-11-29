6ce2eea8172bfa06780f96441ba4d1a141a7335e
anthony2704 <anthony2742000@gmail.com>
2020-11-29 20:57:20 +0700

    fix completed

--- ShopeeCrawler.py
+++ ShopeeCrawler.py
@@ -1,19 +1,22 @@
+import xlwt
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
-
 chrome_options = Options()
-# chrome_options.add_argument("--headless")
 
 chromedriver = '/usr/lib/chromium-browser/chromedriver'
 driver = webdriver.Chrome(chrome_options=chrome_options, executable_path=chromedriver)
 driver.get("https://shopee.vn/Laptop-cat.13030.13065")
-
 all_links = driver.find_elements_by_class_name('_3eufr2')
 
-for x in all_links:
-    print(x.text)
-    print('\n')
+book = xlwt.Workbook(encoding="utf-8")
+sheet1 = book.add_sheet("Sheet 1")
+sheet1.write(0, 0, "ITEM")
+i = 1
+for x in range(len(all_links)):
+    sheet1.write(i, 0, all_links[x].text)
+    i = i + 1
+book.save("data.xlsx")
 
 print('Complete')
 
